42f533260fcd1cab212d13d1cd259630ed487cc4
naariman <pticafugu@gmail.com>
2025-02-15 17:22:13 +0500

    Initial Commit

---
+++ LocaLLM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocaLLM
+//
+//  Created by rbkusser on 15.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocaLLM/LocaLLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocaLLMApp.swift
+//  LocaLLM
+//
+//  Created by rbkusser on 15.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct LocaLLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
